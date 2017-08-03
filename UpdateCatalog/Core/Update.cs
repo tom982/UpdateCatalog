844353a96e6d5530286e1077d1b01bf67037b7df
Tom Hayter <tom.hayter@btconnect.com>
2017-08-03 10:11:40 +0100

    Add project files.

---
+++ UpdateCatalog/Core/Update.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateCatalog.Core
+{
+    public class Update
+    {
+        [JsonProperty(PropertyName = "FooBar")]
+
+        public string kbNumber { get; set; }
+        public string windowsVersion { get; set; }
+        public string downloadLink { get; set; }
+        public string architecture { get; set; }
+        public Cabfile[] cabFiles { get; set; }
+    }
+
+    public class Cabfile
+    {
+        public string filename { get; set; }
+        public string hash { get; set; }
+        public Manifest[] manifests { get; set; }
+        public Package[] packages { get; set; }
+        public Payload[] payloads { get; set; }
+    }
+
+    public class Manifest
+    {
+        public string name { get; set; }
+        public string hash { get; set; }
+    }
+
+    public class Package
+    {
+        public string name { get; set; }
+        public string hash { get; set; }
+    }
+
+    public class Payload
+    {
+        public string name { get; set; }
+        public string hash { get; set; }
+    }
+
+}
---
+++ UpdateCatalog/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UpdateCatalog.Core;
+
+namespace UpdateCatalog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] files = Directory.GetFiles(Assembly.GetExecutingAssembly().Location, "*.msu");
+            foreach (string file in files)
+            {
+                
+            }
+        }
+    }
+}
